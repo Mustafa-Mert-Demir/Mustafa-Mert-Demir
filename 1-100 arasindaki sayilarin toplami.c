5f2b21bed6dfd005ad16421b3f453f78e1bc4a59
Mustafa Mert DEMİR <146746084+Mustafa-Mert-Demir@users.noreply.github.com>
2023-10-02 17:50:22 +0300

    1-100 arası sayıların toplamını sağlayan C kodu

---
+++ 1-100 arasindaki sayilarin toplami.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+	
+	
+	int toplam=0,i;
+	
+	for(i=1;i<=100;i++){
+		
+		toplam=toplam+i;
+		
+		
+	}
+	
+printf("1-100 arasindaki sayilarin toplami: %d",toplam);
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
